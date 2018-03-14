3fffefe7676627ef6bcd5ab76c10c1439014afb4
Serkan BOZTEPE <serkanboztepe02@gmail.com>
2018-03-14 23:27:09 +0300

    Product Request

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
@@ -9,6 +10,11 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware("auth:api")->except("index","show");
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +41,20 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+
+
+        return response([
+            'data' => new ProductResource($product)
+        ],201);
     }
 
     /**
